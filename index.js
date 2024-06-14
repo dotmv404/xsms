af0dfd59b47f8d986899a86725ee89fef9ee5688
dotmv404 <grasslover045@gmail.com>
2024-06-15 04:16:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { textcoffe } = require("./textacoffee");
 const TelegramBot = require("node-telegram-bot-api");
 const token = "5214920125:AAHaRYCvu4wbgZDslHCip-GxZOEmk-1sDS0";
+// const token = "7393849192:AAHMwQvNN6GMW2B7QgX0ebYthyoPvU03ar8";
+
+
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -16,16 +19,10 @@ let message = msg['text'].split(" ")
 if (message[0].length < 5) return
 
 
-async function runLoop(lmsg,lnumber) {
-    for (let i = 1; i <= lnumber; i++) {
-textcoffe(lmsg);
-  bot.sendMessage(chatId, `Attack ${i}  ✅`);
-    
-      await new Promise(resolve => setTimeout(resolve, 2000));
-    }
-  }
 
-  runLoop(message[0],message[1])
+bot.sendMessage(chatId, "Started Attacking");
+  textcoffe(message[0],message[1]);
+
 
 
 });
@@ -45,12 +42,14 @@ function checkIfNumber(value) {
 
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3000; // Choose any port you prefer
 
+// Define a route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
